b104efa6796cc21dcd7e496eb33072baef637e41
tatwd <leoking9641@hotmail.com>
2021-08-20 02:17:24 +0800

    add r2ray setup script

---
+++ setup_r2ray.sh
@@ -0,0 +1,36 @@
+# $config_dir=/user/local/etc/v2ray
+#echo "Create v2ray config folder"
+#sudo mkdir -p $config_dir
+
+ehco "Download v2ray from github and install"
+bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+
+sudo systemctl enable v2ray
+
+cat <<EOF > /usr/local/etc/v2ray/config.json
+{
+    "inbounds": [
+        {
+            "port": 10086,
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "f81913a5-ba26-4042-9822-77e9d2bfabbf"
+                    }
+                ]
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}
+EOF
+
+sudo systemctl start v2ray
+
+sudo firewall-cmd --zone=public --add-port=10086/tcp --permanent
+sudo systemctl restart firewalld
