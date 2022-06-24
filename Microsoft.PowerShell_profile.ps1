04f5ce2fb4afc0455cb03a506892bca71ad6c600
_king <leoking9641@hotmail.com>
2022-06-24 12:22:14 +0800

    update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,10 @@ set-alias zip Compress-Archive
 set-alias trash Remove-ItemSafely
 set-alias which Get-Command
 
+function Test-Command($cmdname) {
+  return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
 function open($file) {
   invoke-item $file
 }
@@ -16,11 +20,11 @@ function lspath {
 }
 
 function get-process-for-port($port) {
-  Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess
+  # `sudo` is installed by scoop
+  sudo Get-Process -Id (Get-NetTCPConnection -LocalPort $port).OwningProcess -IncludeUserName
 }
 
 function goworks {
-  #replace yourself
   set-location 'd:/works'
 }
 
@@ -28,18 +32,44 @@ function psfd($v) {
   Get-Process | where {$_.ProcessName -match $v -or $_.Id -match $v}
 }
 
+function setproxy {
+  #git
+  git config --global http.proxy "http://127.0.0.1:7890"
+  git config --global https.proxy "http://127.0.0.1:7890"
+  echo "set git proxy ok"
+}
+
+function unsetproxy($v) {
+  #git
+  git config --global --unset http.proxy
+  git config --global --unset https.proxy
+  echo "unset git proxy ok"
+}
+
 #set-location 'd:/works'
 
 #try { $null = gcm pshazz -ea stop; pshazz init } catch { }
 #Import-Module posh-git
 #Import-Module oh-my-posh
-
-#$env:RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup"
-$env:RUSTUP_DIST_SERVER="https://mirrors.ustc.edu.cn/rust-static"
-
-
 #Set-Theme Sorin
 #Set-Prompt
 #Set-Theme Agnoster
 
-Invoke-Expression (&starship init powershell)
+if (Test-Command starship) {
+  Invoke-Expression (&starship init powershell)
+}
+
+if (Test-Command scoop.ps1) {
+  $completeModulePath = "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
+  if (Test-Path $completeModulePath) {
+    Import-Module  $completeModulePath
+  }
+}
+
+if (Test-Command rustup) {
+  #$env:RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup"
+  $env:RUSTUP_DIST_SERVER="https://mirrors.ustc.edu.cn/rust-static"
+}
+
+
+
