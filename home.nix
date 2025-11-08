cf4eca4648cb6919ece2f4434858e143169b7dd5
tatwd <tatwdo@gmail.com>
2025-11-08 15:56:14 +0800

    update

--- home.nix
+++ home.nix
@@ -39,15 +39,15 @@ in
     vscode
     google-chrome
     jetbrains.rider
-#    iterm2
-    alacritty
     wezterm
+#    alacritty
+#    tmux
+#    zellij
 #    ghostty
+#    iterm2
     avalonia-ilspy
     ollama
     podman
-#    tmux
-    zellij
 #    godot
     mitmproxy
 #    postman
@@ -150,13 +150,68 @@ in
       "set expandtab " replace tab -> space
     '';
     extraLuaConfig = ''
+      -- Bootstrap lazy.nvim
+      local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+      if not (vim.uv or vim.loop).fs_stat(lazypath) then
+        local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+        local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+        if vim.v.shell_error ~= 0 then
+          vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+          }, true, {})
+          vim.fn.getchar()
+          os.exit(1)
+        end
+      end
+      vim.opt.rtp:prepend(lazypath)
+
+      vim.g.mapleader = " "
+      vim.g.maplocalleader = "\\"
+
       vim.opt.inccommand = 'split'
-      vim.opt.lazyredraw = true    -- 延迟重绘提高性能 
+      vim.opt.lazyredraw = true
+
+      -- Setup lazy.nvim
+      require("lazy").setup({
+        spec = {
+          { "tpope/vim-sleuth" },
+          { "tpope/vim-commentary"  },
+          { "tpope/vim-surround" },
+          { "tpope/vim-repeat" },
+          { 
+            "catppuccin/nvim", 
+            name = "catppuccin", 
+            priority = 1000,
+            opts = {
+              flavour = "mocha",
+              --transparent_background = true,
+            },
+            config = function()
+              vim.cmd.colorscheme("catppuccin")
+            end,
+          },
+          { "nvim-tree/nvim-web-devicons", lazy = true },
+          {
+            "nvim-tree/nvim-tree.lua",
+            opts = {}
+          },
+          {
+            "nvim-lualine/lualine.nvim",
+            opts = { theme = "catppuccin" }
+          }
+        },
+        --install = { colorscheme = { "habamax" } },
+        --checker = { enabled = true },
+      })
+
+      --vim.cmd.colorscheme("catppuccin")
     '';
 
-    plugins = with pkgs.vimPlugins; [
-      vim-sleuth
-    ];
+#    plugins = with pkgs.vimPlugins; [
+#      vim-sleuth
+#    ];
 
   };
 
--- wezterm.lua
+++ wezterm.lua
@@ -6,13 +6,13 @@ config.initial_cols = 92
 config.initial_rows = 30
 
 config.window_decorations = "RESIZE"
---config.window_background_opacity = 0.9
+config.window_background_opacity = 0.98
 --config.macos_window_background_blur = 20
 config.window_padding = {
---  left = 2,
---  right = 2,
---  top = 10,
-  bottom = 5,
+  left = '1cell',
+  right = '1cell',
+  top = '0.5cell',
+  bottom = '0.5cell',
 }
 --config.set_environment_variables = {
 --  COLORTERM = "truecolor",
@@ -35,10 +35,10 @@ config.keys = {
   { key = '|', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
   { key = '-', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
   { key = 'z', mods = 'LEADER', action = wezterm.action.TogglePaneZoomState },
-  { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
-  { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
-  { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
-  { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
+--  { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
+--  { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
+--  { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
+--  { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
   { key = 'x', mods = 'LEADER', action = wezterm.action.CloseCurrentPane { confirm = true } },
   { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
   { key = '?', mods = 'LEADER', action = wezterm.action.ShowLauncher },
