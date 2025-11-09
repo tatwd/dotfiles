285d1b24e3ef198f272fac053935283621031891
tatwd <tatwdo@gmail.com>
2025-11-09 20:07:42 +0800

    update

--- mac-nix.sh
+++ mac-nix.sh
@@ -1,5 +1,7 @@
 sh <(curl https://mirrors.tuna.tsinghua.edu.cn/nix/latest/install)
 
 nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+# nix-channel --add https://gh.llkk.cc/https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 
 nix-channel --update
